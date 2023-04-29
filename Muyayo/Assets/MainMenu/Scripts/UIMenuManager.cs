e491f0d212fb9e0fb8c47aff087f52a151dcb296
TaigoroCortez <taigorocortez@gmail.com>
2023-04-29 19:13:43 +0200

    main menu
    animacion de paneles menu

---
+++ Muyayo/Assets/MainMenu/Scripts/UIMenuManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIMenuManager : MonoBehaviour
+{
+    public RectTransform mainMenu, selectGame, setting,credits;
+    float timeAnimation = 0.25f;
+    void Start()
+    {
+        mainMenu.DOAnchorPos(Vector2.zero, 0.25f);
+        
+    }
+
+
+    public void ButtonSelectGame()
+    {
+        mainMenu.DOAnchorPos(new Vector2(-2000,0), timeAnimation);
+        selectGame.DOAnchorPos(new Vector2(0, 0), timeAnimation);
+    }
+
+    public void ButtonBackSelectGame()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0,0), timeAnimation);
+        selectGame.DOAnchorPos(new Vector2(2000,0), timeAnimation);
+    }
+
+    public void ButtonSetting()
+    {
+        mainMenu.DOAnchorPos(new Vector2(-2000, 0), timeAnimation);
+        setting.DOAnchorPos(new Vector2(0, 0), timeAnimation);
+    }
+
+    public void ButtonBackSetting()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, 0), timeAnimation);
+        setting.DOAnchorPos(new Vector2(0, 3550), timeAnimation);
+    }
+
+    public void ButtonCredits()
+    {
+        mainMenu.DOAnchorPos(new Vector2(-2000, 0), timeAnimation);
+        credits.DOAnchorPos(new Vector2(0, 0), timeAnimation);
+    }
+
+    public void ButtonBackCredits()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, 0), timeAnimation);
+        credits.DOAnchorPos(new Vector2(0, -3550), timeAnimation);
+    }
+}
