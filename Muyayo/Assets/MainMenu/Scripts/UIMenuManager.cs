61698bf1d74a51f8378877ab73c59e2115dddf47
TaigoroCortez <taigorocortez@gmail.com>
2023-05-01 21:02:58 +0200

    Main Menu test 2
    correccion de menu principal

--- Muyayo/Assets/MainMenu/Scripts/UIMenuManager.cs
+++ Muyayo/Assets/MainMenu/Scripts/UIMenuManager.cs
@@ -6,15 +6,15 @@ using DG.Tweening;
 
 public class UIMenuManager : MonoBehaviour
 {
-    public RectTransform mainMenu, selectGame, setting,credits;
+    public RectTransform mainMenu, selectGame, setting, credits;
     float timeAnimation = 0.25f;
     void Start()
     {
         mainMenu.DOAnchorPos(Vector2.zero, 0.25f);
-        
+
     }
 
-
+    #region ButtonMainMenu
     public void ButtonSelectGame()
     {
         mainMenu.DOAnchorPos(new Vector2(-2000,0), timeAnimation);
@@ -36,7 +36,7 @@ public class UIMenuManager : MonoBehaviour
     public void ButtonBackSetting()
     {
         mainMenu.DOAnchorPos(new Vector2(0, 0), timeAnimation);
-        setting.DOAnchorPos(new Vector2(0, 3550), timeAnimation);
+        setting.DOAnchorPos(new Vector2(0, 4200), timeAnimation);
     }
 
     public void ButtonCredits()
@@ -50,4 +50,5 @@ public class UIMenuManager : MonoBehaviour
         mainMenu.DOAnchorPos(new Vector2(0, 0), timeAnimation);
         credits.DOAnchorPos(new Vector2(0, -3550), timeAnimation);
     }
+    #endregion
 }
